0c13b707542564138b1ac2002c8a9256f9bba9c9
DCalderonMejia <125091380+DCalderonMejia@users.noreply.github.com>
2023-05-15 16:01:32 -0500

    solucion a los ejercicios de js

---
+++ js-1/hello.js
@@ -0,0 +1,8 @@
+function hola() {
+    return "Hola !";
+}
+
+// código de prueba
+console.log(hola("Pedro")) // "Hola Pedro!"
+console.log(hola("Juan")) // "Hola Juan!"
+console.log(hola("")) // "Hola !"
---
+++ js-1/sum.js
@@ -0,0 +1,8 @@
+function sum (num1) {
+    return num1;
+}
+
+// código de prueba
+console.log(sum(4)) // 1 + 2 + 3 + 4 = 10
+console.log(sum(10)) // 55
+console.log(sum(15)) // 120
